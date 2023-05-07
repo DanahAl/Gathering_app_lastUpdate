387e594cf8c804c68af11912906e2b3cff2b07a3
DanahAl <ddanah.mm@gmail.com>
2023-05-07 21:47:38 +0300

    Log out work!

--- app/src/main/java/com/example/gatheringapp/home2.java
+++ app/src/main/java/com/example/gatheringapp/home2.java
@@ -10,23 +10,32 @@ import android.widget.Button;
 
 public class home2 extends AppCompatActivity {
     private Button b_add;
+    private Button LogOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
 
         b_add = (Button) findViewById(R.id.b_add);
-
+        LogOut = (Button) findViewById(R.id.buttonLogOut);
         b_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openAdd();
             }
         });
+
+        LogOut.setOnClickListener( new View.OnClickListener() {public void onClick(View view) {
+            openHome();
+        }});
     }
 
     public void openAdd(){
         Intent intent = new Intent(this, AddChalet.class);
+        startActivity(intent);
+    }
+    public void openHome(){
+        Intent intent = new Intent(this, home.class);
         startActivity(intent);
     }
 }
